575544f3a9a539962007bfac40a4e02fdead9374
Henry Zhang <yuz087@ucsd.edu>
2020-01-16 12:15:10 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,9 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $('a.thumbnail').click(projectClick);
+}
+function projectClick(e) {
+  e.preventDefault();
+  $(this).css("background-color", "#7fff00");
 }
